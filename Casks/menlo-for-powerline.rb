732aa5fe6ccf9ac9c79cb756838a9152028ff26a
Josa Gesell <josa@gesell.me>
2014-10-28 14:14:57 +0100

    initial commit

---
+++ Casks/menlo-for-powerline.rb
@@ -0,0 +1,13 @@
+class MenloForPowerline < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/abertsch/Menlo-for-Powerline/archive/master.zip'
+  homepage 'https://github.com/abertsch/Menlo-for-Powerline'
+  license :closed
+
+  font 'Menlo-for-Powerline-master/Menlo Bold Italic for Powerline.ttf'
+  font 'Menlo-for-Powerline-master/Menlo Bold for Powerline.ttf'
+  font 'Menlo-for-Powerline-master/Menlo Italic for Powerline.ttf'
+  font 'Menlo-for-Powerline-master/Menlo for Powerline.ttf'
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+# Caskroom-josa-fonts
+
+See:
+* [caskroom/homebrew-cask](https://github.com/caskroom/homebrew-cask)
+* [caskroom/homebrew-fonts](https://github.com/caskroom/homebrew-fonts)
