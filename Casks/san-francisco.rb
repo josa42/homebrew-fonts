8d8de8011a5a244719856dae96364a322a85e959
Josa Gesell <josa.gesell@ligadigital.com>
2016-12-03 11:44:51 +0100

    Add San Fancisco

---
+++ Casks/san-francisco.rb
@@ -0,0 +1,30 @@
+cask 'san-francisco' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codeload.github.com/AppleDesignResources/SanFranciscoFont/zip/master'
+  homepage 'https://github.com/AppleDesignResources/SanFranciscoFont'
+  license :closed
+
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Black.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Bold.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Heavy.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Light.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Medium.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Semibold.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Thin.otf'
+  font 'SanFranciscoFont-master/SanFranciscoDisplay-Ultralight.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Bold.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-BoldItalic.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Heavy.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-HeavyItalic.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Light.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-LightItalic.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Medium.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-MediumItalic.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Regular.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-RegularItalic.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-Semibold.otf'
+  font 'SanFranciscoFont-master/SanFranciscoText-SemiboldItalic.otf'
+end
